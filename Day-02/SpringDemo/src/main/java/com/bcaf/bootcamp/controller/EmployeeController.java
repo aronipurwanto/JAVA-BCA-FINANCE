971f92f724e84e2c443a81e9419b2a8c34d9e319
ahmadroni <ahmadroni@gmail.com>
2019-06-25 14:58:40 +0700

    update model jobtitle dan department

--- Day-02/SpringDemo/src/main/java/com/bcaf/bootcamp/controller/EmployeeController.java
+++ Day-02/SpringDemo/src/main/java/com/bcaf/bootcamp/controller/EmployeeController.java
@@ -1,24 +1,55 @@
 package com.bcaf.bootcamp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bcaf.bootcamp.model.EmployeeModel;
 
 @Controller
 public class EmployeeController {
 
-	@GetMapping(value="/employee")
+	@GetMapping(value="/employee") // url
 	public String index() {
-		return "employee/index";
+		return "employee/index"; // view
 	}
 	
-	@GetMapping(value="/employee/create")
+	@GetMapping(value="/employee/create") // url
 	public String create() {
-		return "employee/create";
+		return "employee/create"; // view
 	}
 	
-	@PostMapping(value="/employee/save")
-	public String save() {
-		return "employee/save";
+	@PostMapping(value="/employee/save") //url
+	public String save(HttpServletRequest request, Model model) {
+		// request untuk menerima data dari form employee/create
+		String id = request.getParameter("id");
+		String nama = request.getParameter("nama");
+		String tptLahir = request.getParameter("tptLahir");
+		String alamat = request.getParameter("alamat");
+		String tglLahir = request.getParameter("tglLahir");
+		
+		// model untuk mengirim data dari controller ke view
+		model.addAttribute("vid", id);
+		model.addAttribute("vnama", nama);
+		model.addAttribute("vtptLahir", tptLahir);
+		model.addAttribute("valamat", alamat);
+		model.addAttribute("vtglLahir", tglLahir);
+		
+		return "employee/save"; // view
+	}
+	
+	@GetMapping(value="/employee/baru") // url
+	public String baru() {
+		return "employee/add"; // view
+	}
+	
+	@PostMapping(value="/employee/simpan")
+	public String simpan(@ModelAttribute EmployeeModel emp, Model model) {
+		model.addAttribute("vemp", emp);
+		return "employee/simpan";
 	}
 }
---
+++ Day-02/SpringDemo/src/main/java/com/bcaf/bootcamp/model/EmployeeModel.java
@@ -0,0 +1,61 @@
+package com.bcaf.bootcamp.model;
+
+public class EmployeeModel {
+	private Integer id;
+	private String nama;
+	private String alamat;
+	private String tptLahir;
+	private String tglLahir;
+	private String agama;
+	private String gender;
+	private String golDarah;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getTptLahir() {
+		return tptLahir;
+	}
+	public void setTptLahir(String tptLahir) {
+		this.tptLahir = tptLahir;
+	}
+	public String getTglLahir() {
+		return tglLahir;
+	}
+	public void setTglLahir(String tglLahir) {
+		this.tglLahir = tglLahir;
+	}
+	public String getAgama() {
+		return agama;
+	}
+	public void setAgama(String agama) {
+		this.agama = agama;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getGolDarah() {
+		return golDarah;
+	}
+	public void setGolDarah(String golDarah) {
+		this.golDarah = golDarah;
+	}
+}
