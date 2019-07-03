8862249c8093c593855df91eed149c4e81ae5063
ahmadroni <ahmadroni@gmail.com>
2019-07-03 16:08:56 +0700

    update hapus Provinsi

--- Day-08/BCAFProject/src/main/java/com/bcaf/project/controller/ProvinsiController.java
+++ Day-08/BCAFProject/src/main/java/com/bcaf/project/controller/ProvinsiController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bcaf.project.model.Kota;
 import com.bcaf.project.model.Provinsi;
+import com.bcaf.project.repository.KotaRepo;
 import com.bcaf.project.repository.ProvinsiRepo;
 
 @Controller
@@ -20,6 +22,9 @@ import com.bcaf.project.repository.ProvinsiRepo;
 public class ProvinsiController {
 	@Autowired
 	private ProvinsiRepo repo;
+	
+	@Autowired
+	private KotaRepo kotaRepo;
 	
 	@GetMapping(value="index")
 	public ModelAndView index() {
@@ -66,6 +71,27 @@ public class ProvinsiController {
 		List<Provinsi> list = this.repo.findAll();
 		view.addObject("list", list);
 		
+		return view;
+	}
+	
+	@GetMapping(value="hapus/{id}")
+	public ModelAndView hapus(@PathVariable("id") Long id) {
+		ModelAndView view = new ModelAndView("provinsi/_form");
+		Provinsi  item = this.repo.findById(id).orElse(null);
+		view.addObject("provinsi", item);
+		return view;
+	}
+	
+	@PostMapping(value="remove")
+	public ModelAndView remove(@ModelAttribute Provinsi provinsi) {
+		//#1 check data di kota
+		if(this.kotaRepo.findByProvinsiId(provinsi.getId()).size()==0) {
+			// jika tidak ada data provinsi di kota, maka proses hapus dilakukan
+			this.repo.delete(provinsi);
+		}
+		
+		ModelAndView view = new ModelAndView("provinsi/_form");
+		view.addObject("kota", new Kota());
 		return view;
 	}
 }
--- Day-08/BCAFProject/src/main/java/com/bcaf/project/repository/KotaRepo.java
+++ Day-08/BCAFProject/src/main/java/com/bcaf/project/repository/KotaRepo.java
@@ -1,5 +1,7 @@
 package com.bcaf.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.bcaf.project.model.Kota;
 
 @Repository
 public interface KotaRepo extends JpaRepository<Kota, Long> {
-
+	public List<Kota> findByProvinsiId(Long provinsiId);
 }
